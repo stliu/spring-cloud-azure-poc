5049801db0b327e3d2efb96fd43f1e5f01a38feb
Xiaolu Dai <xiada@microsoft.com>
2021-08-25 13:25:43 +0800

    add test for http client

--- spring-cloud-azure-autoconfigure/src/test/java/com/azure/spring/autoconfigure/storage/blob/AzureBlobClientBuilderFactoryTest.java
+++ spring-cloud-azure-autoconfigure/src/test/java/com/azure/spring/autoconfigure/storage/blob/AzureBlobClientBuilderFactoryTest.java
@@ -1,5 +1,10 @@
 package com.azure.spring.autoconfigure.storage.blob;
 
+import com.azure.core.http.HttpClient;
+import com.azure.core.http.HttpClientProvider;
+import com.azure.core.http.HttpRequest;
+import com.azure.core.http.HttpResponse;
+import com.azure.core.util.Context;
 import com.azure.identity.ClientCertificateCredential;
 import com.azure.identity.ClientSecretCredential;
 import com.azure.spring.core.properties.credential.TokenCredentialProperties;
@@ -7,6 +12,7 @@ import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobClientBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
@@ -48,8 +54,10 @@ public class AzureBlobClientBuilderFactoryTest {
         tokenCredentialProperties.setTenantId("test-tenant");
         tokenCredentialProperties.setClientId("test-client");
         tokenCredentialProperties.setClientSecret("test-secret");
+
         final BlobClientBuilder blobClientBuilder = new AzureBlobClientBuilderFactoryExt(properties).build();
         final BlobClient blobClient = blobClientBuilder.buildClient();
+
         verify(builder, times(1)).credential(any(ClientSecretCredential.class));
     }
 
@@ -65,9 +73,47 @@ public class AzureBlobClientBuilderFactoryTest {
         tokenCredentialProperties.setClientId("test-client");
         tokenCredentialProperties.setCertificatePath("test-cert-path");
         tokenCredentialProperties.setCertificatePassword("test-cert-password");
+
         final BlobClientBuilder blobClientBuilder = new AzureBlobClientBuilderFactoryExt(properties).build();
         final BlobClient blobClient = blobClientBuilder.buildClient();
+
         verify(builder, times(1)).credential(any(ClientCertificateCredential.class));
+    }
+
+    @Test
+    public void testSetHttpClient() {
+        AzureStorageBlobProperties properties = new AzureStorageBlobProperties();
+        properties.setEndpoint(ENDPOINT);
+        properties.setBlobName(BLOB_NAME);
+
+        final AzureBlobClientBuilderFactoryExt builderFactory = new AzureBlobClientBuilderFactoryExt(properties);
+
+        builderFactory.setHttpClientProvider(new TestHttpClientProvider());
+        final BlobClientBuilder blobClientBuilder = builderFactory.build();
+        final BlobClient blobClient = blobClientBuilder.buildClient();
+
+        verify(builder, times(1)).httpClient(any(TestHttpClient.class));
+    }
+
+    static class TestHttpClientProvider implements HttpClientProvider {
+
+        @Override
+        public HttpClient createInstance() {
+            return new TestHttpClient();
+        }
+    }
+
+    static class TestHttpClient implements HttpClient {
+
+        @Override
+        public Mono<HttpResponse> send(HttpRequest request) {
+            return null;
+        }
+
+        @Override
+        public Mono<HttpResponse> send(HttpRequest request, Context context) {
+            return HttpClient.super.send(request, context);
+        }
     }
 
     static class AzureBlobClientBuilderFactoryExt extends AzureBlobClientBuilderFactory {
